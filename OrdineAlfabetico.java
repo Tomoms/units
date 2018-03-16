b5aa7c718926fb9bbb53a68168325a659a722d3c
Andrea Gonzato <gonzatoandrea98@gmail.com>
2018-03-16 20:58:26 +0100

    Comments

--- OrdineAlfabetico.java
+++ OrdineAlfabetico.java
@@ -2,39 +2,66 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-class OrdineAlfabetico {
-	//commento by Andrea
-	// modifica 2
+/*
+Created 15/3/18
 
-  public static void main (String[] args)
-  {
+16/3 : Comments
+*/
+
+class OrdineAlfabetico {
+
+  public static void main (String[] args){
+	  
       String[] input = inputData();
+	  
+	  //TESTING: view "myArray"
+	  System.out.println("INPUT: "+ Arrays.toString(input));
+	  
+	  
+	  
   }
 
-    static String[] inputData()
-  {
-      ArrayList<String> input = new ArrayList<String>();
-      Scanner sc = new Scanner(System.in);
+  
+   private static String[] inputData(){
+	   
+      ArrayList<String> input = new ArrayList<String>(); //create an ArrayList
+      Scanner sc = new Scanner(System.in); // create a Scanner
+	  
       int count = -1;
 
         do {
-          System.out.println("Inserisci un vocabolo/numero: ");
+          System.out.println("Enter a alphanumeric word:");
           String data = sc.nextLine();
-          data = data.trim();
-          data = data.toLowerCase();
-          input.add(data);
+		  
+		  data = clean(data); //clean data
+		  
+          input.add(data); //add data to ArrayList
+		  
           count++;
       } while (!input.get(count).equals(""));
 
-      String[] array = input.toArray(new String[0]);
-      array = Arrays.copyOf(array, array.length - 1);
-      return array;
-  }
+      String[] myArray = input.toArray(new String[0]); //copy the ArrayList in to a new Array "myArray"
+	  
+      myArray = Arrays.copyOf(myArray, myArray.length - 1); //remove the last cell from "myArray"
+	  
+      return myArray;
+	}
+	
 
+	private static String clean (String toClean) {
+		
+		toClean = toClean.trim();
+        toClean = toClean.toLowerCase();
+		
+		return toClean;
+	}
+	
+	
 /*
-    static String[] ordina(String[] data)
-  {
-
-  }
+    private static String order2String(String data1, String data2) {
+		
+	}
 */
+
+
 }
