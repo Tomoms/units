a9445ab3d841975a40a3f58b6cda86a6ee424161
Andrea Gonzato <gonzatoandrea98@gmail.com>
2018-03-19 16:50:22 +0100

    No insertion Sort

--- OrdineAlfabetico.java
+++ OrdineAlfabetico.java
@@ -9,59 +9,89 @@ Created 15/3/18
 */
 
 class OrdineAlfabetico {
+	
+	final static String allChars = "0123456789aàbcdeèéfghiìjklmnoòpqrstuùvwxyz";
 
-  public static void main (String[] args){
+	public static void main (String[] args){
 	  
-      String[] input = inputData();
+		String[] input = inputData();
 	  
-	  // TESTING: print "myArray"
-	  System.out.println("INPUT: "+ Arrays.toString(input));
+		// TESTING: print "myArray"
+		System.out.println("INPUT: "+ Arrays.toString(input));
+		
+		//TESTING
+		System.out.println(order2Strings(input[0], input[1]));
 	  
 	  
 	  
-  }
+	}
 
   
-   private static String[] inputData(){
+	private static String[] inputData(){
 	   
-      ArrayList<String> input = new ArrayList<String>(); // create an ArrayList
-      Scanner sc = new Scanner(System.in); // create a Scanner
+		ArrayList<String> input = new ArrayList<String>(); // create an ArrayList
+		Scanner sc = new Scanner(System.in); // create a Scanner
 	  
-      int count = -1;
+		int count = -1;
 
-        do {
-          System.out.println("Enter an alphanumeric string:");
-          String data = sc.nextLine();
+		do {
+			System.out.println("Enter an alphanumeric string:");
+			String data = sc.nextLine();
 		  
-		  data = clean(data); // clean data
-		  
-          input.add(data); // add data to ArrayList
-		  
-          count++;
-      } while (!input.get(count).equals(""));
+			data = clean(data); // clean data
+			if(checkString(data)) {
+				input.add(data); // add data to ArrayList
+				count++;
+			} else System.out.println("\nERROR: The String contains a forbidden character, thus it will be ignored\n");
+			
+		} while (!input.get(count).equals(""));
 
-      String[] myArray = input.toArray(new String[0]); // copy the ArrayList into a new Array called "myArray"
+		String[] myArray = input.toArray(new String[0]); // copy the ArrayList into a new Array called "myArray"
 	  
-      myArray = Arrays.copyOf(myArray, myArray.length - 1); // remove the last cell from "myArray"
+		myArray = Arrays.copyOf(myArray, myArray.length - 1); // remove the last cell from "myArray"
 	  
-      return myArray;
-	}
+		return myArray;
+		}
 	
 
-	private static String clean (String toClean) {
+		private static String clean (String toClean) {
 		
-		toClean = toClean.trim();
-        toClean = toClean.toLowerCase();
+			toClean = toClean.trim();
+			toClean = toClean.toLowerCase();
+			
+				
 		
-		return toClean;
-	}
+			return toClean;
+		}
 	
-	
-/*
-    private static String order2String(String data1, String data2) {
+		private static boolean checkString(String arg) {
+			
+			for (int i=0 ; i<arg.length() ; i++) {
+				if(allChars.indexOf(arg.charAt(i)) == -1) return false;
+			}
+			return true;
+		}
 		
-	}
-*/
+		//TRUE: if s1=a & s2=z
+		private static boolean order2Strings(String s1, String s2) {
+		
+			int minLength = (s1.length() <= s2.length()) ? s1.length() : s2.length();
+
+			if(s1.equals(s2)) return true;
+			
+			for (int i=0 ; i<minLength ; i++) {
+			
+				if(allChars.indexOf(s1.charAt(i)) < allChars.indexOf(s2.charAt(i))) return true;
+				else if (allChars.indexOf(s1.charAt(i)) < allChars.indexOf(s2.charAt(i))) return false;
+			
+			}
+			
+			if(s1.length() < s2.length()) return true;
+			else return false;
+		}
+		
+		
 
 
-}
+
+	}
