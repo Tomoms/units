bd7b9678513c929391dd22d35f4050ee98da276c
G0nza98 <32014520+G0nza98@users.noreply.github.com>
2019-03-14 22:26:58 +0100

    ArrayList Control

--- TextReader.java
+++ TextReader.java
@@ -1,18 +1,54 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 class TextReader {
+	
+	private static final String FILENAME = "C:\\Users\\Andrea\\Documents\\code\\java\\Lettore_di_file\\Text.txt";
 
 	public static void main(String[] args) throws java.io.IOException {
-		int i;
-		FileReader fr;
+		BufferedReader br = null;
+		FileReader fr = null;
+
 		try {
-			fr = new FileReader(args[0]);
-		} catch (java.io.FileNotFoundException e) {
-			System.out.println("File non trovato!");
-			return;
+
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+			ArrayList<String> lines = new ArrayList<String>();
+			String sCurrentLine;
+			
+			while ((sCurrentLine = br.readLine()) != null) {
+				lines.add(sCurrentLine);
+				System.out.println(sCurrentLine);
+			}
+			
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
 		}
-		while ((i = fr.read()) != -1)
-			System.out.print((char) i);
+	
 	}
 
 }
